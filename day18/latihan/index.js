8062e12a1dd6d48f11118c3ffab8f12134efebec
MyWahyuSap <wahyusaputra222000@gmail.com>
2023-10-19 15:35:16 +0700

    19/10/2023

--- day18/latihan/index.js
+++ day18/latihan/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+// import module bernama `os`
+const os = require('os');
+const luasSegitiga = require('./segitiga.js');
+
+//import module bernama `fs`
+const fs = require('fs');
+const isi = fs.readFileSync('./text.txt', 'utf-8');
+
+console.log('Free Memory: ', os.freemem());
+
+console.log('Luas Segitiga: ', luasSegitiga(4, 6));
+
+console.log('Isi File: ', isi);
